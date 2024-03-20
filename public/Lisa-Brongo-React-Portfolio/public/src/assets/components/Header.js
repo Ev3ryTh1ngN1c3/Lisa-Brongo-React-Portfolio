1287d5f0d06bca2e7c00f433d0e13d0ae69bc24c
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-03-19 22:53:51 -0400

    added header content

--- public/Lisa-Brongo-React-Portfolio/public/src/assets/components/Header.js
+++ public/Lisa-Brongo-React-Portfolio/public/src/assets/components/Header.js
@@ -1,28 +1,34 @@
-import {Link} from "react-router-dom"
+import React from "react";
+import PropTypes from 'prop-types';
+import Nav from "../Nav";
 
-function Header() {
-    return (
-      <section id="about-me" className="my-5 intro">
-       
-       <h2>This is the Header</h2>
-
-       <ul className="nav-ul">
-        <li>
-          <Link to="/">About Me</Link>
-        </li>
-        <li>
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/resume">Resume</Link>
-        </li>
-       </ul>
-       
+// Header component that displays the header section of the portfolio
+function Header({ currentTab, handleTabChange }) {
+  return (
+    <header>
+      <div className="header-content">
+        <h1>Lisa Brongo</h1> {/* Displaying the name in the header */}
+        <Nav currentTab={currentTab} handleTabChange={handleTabChange} /> {/* Rendering the navigation component */}
+      </div>
+      <section className="hero">
+        <div className="hero-cta">
+          <h2>welcome !</h2> {/* Displaying a welcome message */}
+          <p>
+            to my portfolio built with React
+            ... as a full stack web developer
+            here, you can find a small sample of the projects I worked on
+            as a UCF Coding Boot Camp student
+          </p> {/* Providing a brief description of the portfolio */}
+        </div>
       </section>
-    );
-  }
-  
-  export default Header;
+    </header>
+  );
+}
+
+// PropTypes for type-checking
+Header.propTypes = {
+  currentTab: PropTypes.string.isRequired, // Requires currentTab as a string
+  handleTabChange: PropTypes.func.isRequired // Requires handleTabChange as a function
+};
+
+export default Header;
