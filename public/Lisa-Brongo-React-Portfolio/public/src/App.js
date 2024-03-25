db2f4b5c6aff3b99344ddd45d884444325f0badc
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-03-24 22:19:33 -0400

    CSS styles: adjust margins, padding, colors & font-family for improved layout & readability.

--- public/Lisa-Brongo-React-Portfolio/public/src/App.js
+++ public/Lisa-Brongo-React-Portfolio/public/src/App.js
@@ -1,30 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import About from './assets/components/About';
-import Portfolio from './assets/components/Portfolio';
-import Resume from './assets/components/Resume';
-import Contact from './assets/components/Contact';
-import Header from './assets/components/Header';
-import Footer from './assets/components/Footer';
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import React, { useState } from "react";
+import Header from "../src/components/Header";
+import About from "../src/components/About";
+import Contact from "../src/components/Contact";
+import Portfolio from "../src/components/Portfolio";
+import Resume from "../src/components/Resume";
+import Footer from "../src/components/Footer";
+import { Helmet } from "react-helmet";
+import "./App.css";
 
 function App() {
+  // State to manage the current tab
+  const [currentTab, setCurrentTab] = useState("About");
+
+  // Method to render the current tab content
+  const renderTab = () => {
+    switch (currentTab) {
+      case "About":
+        return <About />;
+      case "Contact":
+        return <Contact />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Resume":
+        return <Resume />;
+      default:
+        return <About />;
+    }
+  };
+
   return (
-    <div className="App">
-      <BrowserRouter>
-
-      <Header />
-
-    <Routes>
-     <Route path="/" element={<About/> }/>
-     <Route path="/portfolio" element={<Portfolio/>}/>
-     <Route path="/contact" element={<Contact />}/>
-     <Route path="/resume" element={<Resume />}/>
-    </Routes>
-
-     <Footer />
-     </BrowserRouter>
-    </div>
+    <>
+      <Helmet>
+        <title>Lisa Brongo's Portfolio | {currentTab}</title>
+      </Helmet>
+      <Header
+        currentTab={currentTab}
+        handleTabChange={setCurrentTab}
+      />
+      <main>{renderTab()}</main>
+      <Footer />
+    </>
   );
 }
 
